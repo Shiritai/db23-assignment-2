6ecb5901f89071bb33b8bd65a1c6d82e385a8e6e
Mnu <132yellowcfyam@gmail.com>
2023-03-23 18:42:43 +0800

    As2UpdatePriceParamHelper Complete

--- bench/src/main/java/org/vanilladb/bench/benchmarks/as2/rte/As2UpdatePriceParamGen.java
+++ bench/src/main/java/org/vanilladb/bench/benchmarks/as2/rte/As2UpdatePriceParamGen.java
@@ -1,23 +1,42 @@
 package org.vanilladb.bench.benchmarks.as2.rte;
 
+import java.util.ArrayList;
+
+import org.vanilladb.bench.benchmarks.as2.As2BenchConstants;
 import org.vanilladb.bench.benchmarks.as2.As2BenchTransactionType;
 import org.vanilladb.bench.rte.TxParamGenerator;
+import org.vanilladb.bench.util.BenchProperties;
+import org.vanilladb.bench.util.RandomValueGenerator;
 
 /**
  * Parameter generator for UpdateItemPriceTxn
  */
 public class As2UpdatePriceParamGen implements TxParamGenerator<As2BenchTransactionType> {
 
-    @Override
+    // Update Counts
+	private static final int TOTAL_UPDATE_COUNT;
+	
+	static {
+		TOTAL_UPDATE_COUNT = BenchProperties.getLoader()
+				.getPropertyAsInteger(As2UpdatePriceParamGen.class.getName() + ".TOTAL_UPDATE_COUNT", 10);
+	}
+	
+	@Override
     public As2BenchTransactionType getTxnType() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getTxnType'");
+        return As2BenchTransactionType.UPDATE_PRICE;
     }
 
     @Override
     public Object[] generateParameter() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'generateParameter'");
+       RandomValueGenerator rvg = new RandomValueGenerator();
+       ArrayList<Object> paramList = new ArrayList<Object>();
+       
+       // Set r count
+		paramList.add(TOTAL_UPDATE_COUNT);
+		for (int i = 0; i < TOTAL_UPDATE_COUNT; i++)
+			paramList.add(rvg.randomDoubleIncrRange(0.0, 5.0, 1000.0));
+
+		return paramList.toArray(new Object[0]);
     }
     
 }
