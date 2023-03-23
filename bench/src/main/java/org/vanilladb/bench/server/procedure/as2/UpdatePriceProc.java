658f3edf67222fdfd6b1c2a151d0740a1d2d8ebc
Shiritai <shingekinocore@gmail.com>
2023-03-23 20:47:09 +0800

    Revision to UpdatePriceProc
    Modify path/typo issues and add two TODOs for reviewing

--- bench/src/main/java/org/vanilladb/bench/server/procedure/as2/UpdatePriceProc.java
+++ bench/src/main/java/org/vanilladb/bench/server/procedure/as2/UpdatePriceProc.java
@@ -1,8 +1,12 @@
 package org.vanilladb.bench.server.procedure.as2;
 
+import org.vanilladb.bench.benchmarks.as2.As2BenchConstants;
 import org.vanilladb.bench.server.param.as2.UpdatePriceProcParamHelper;
+import org.vanilladb.bench.server.procedure.StoredProcedureHelper;
+import org.vanilladb.core.query.algebra.Scan;
 import org.vanilladb.core.sql.storedprocedure.StoredProcedure;
-import org.vanilladb.bench.benchmarks.as2;
+import org.vanilladb.core.storage.tx.Transaction;
+
 /**
  * Implementation of Stored Procedure of UpdateItemPriceTxn
  */
@@ -20,7 +24,7 @@ public class UpdatePriceProc extends StoredProcedure<UpdatePriceProcParamHelper>
 
         for (int idx = 0; idx < paramHelper.getUpdateCount(); idx++){
             int iid = paramHelper.getUpdateItemId(idx);
-            float priceRaise = paramHelper.getPriceRaise(idx);
+            float priceRaise = paramHelper.getPriceRaise(idx); // TODO: perhaps "double" ?
             double price;
             Scan s = StoredProcedureHelper.executeQuery(
                 "SELECT i_name, i_price FROM item WHERE i_id = " + iid,
@@ -38,7 +42,7 @@ public class UpdatePriceProc extends StoredProcedure<UpdatePriceProcParamHelper>
             s.close();
             double newPrice = priceRaise + price;
             int count;
-            if (newPrice > As2BenchConstants.MAXPRICE){
+            if (newPrice > As2BenchConstants.MAX_PRICE){
                 count = StoredProcedureHelper.executeUpdate(
                     "UPDATE item SET i_price = " + As2BenchConstants.MIN_PRICE + "WHERE i_id = " + iid,
                     tx
@@ -48,9 +52,9 @@ public class UpdatePriceProc extends StoredProcedure<UpdatePriceProcParamHelper>
                     "UPDATE item SET i_price = " + newPrice + "WHERE i_id = " + iid,
                     tx
                 );
-            } else 
+            } else // TODO  fix this (double else)
                 throw new RuntimeException("Cloud not update item record with i_id = " + iid);
         }
     }
-    
+
 }
