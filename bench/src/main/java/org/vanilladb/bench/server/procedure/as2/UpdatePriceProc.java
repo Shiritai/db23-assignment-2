088fae51b2ec2fc2e6e0cef382165cd04d66c879
Sunny <sunnyhong91@gmail.com>
2023-03-24 15:52:26 +0800

    Fix type error and some logical bugs on raising price

--- bench/src/main/java/org/vanilladb/bench/server/procedure/as2/UpdatePriceProc.java
+++ bench/src/main/java/org/vanilladb/bench/server/procedure/as2/UpdatePriceProc.java
@@ -24,7 +24,7 @@ public class UpdatePriceProc extends StoredProcedure<UpdatePriceProcParamHelper>
 
         for (int idx = 0; idx < paramHelper.getUpdateCount(); idx++){
             int iid = paramHelper.getUpdateItemId(idx);
-            float priceRaise = paramHelper.getPriceRaise(idx); // TODO: perhaps "double" ?
+            double priceRaise = paramHelper.getPriceRaise(idx);
             double price;
             Scan s = StoredProcedureHelper.executeQuery(
                 "SELECT i_name, i_price FROM item WHERE i_id = " + iid,
@@ -34,26 +34,20 @@ public class UpdatePriceProc extends StoredProcedure<UpdatePriceProcParamHelper>
 			if (s.next()) {
 				String name = (String) s.getVal("i_name").asJavaVal();
 				price = (Double) s.getVal("i_price").asJavaVal();
-
 				paramHelper.setItemName(name, idx);
 				paramHelper.setItemPrice(price, idx);
 			} else
-				throw new RuntimeException("Cloud not find item record with i_id = " + iid);
+				throw new RuntimeException("Could not find item record with i_id = " + iid);
             s.close();
             double newPrice = priceRaise + price;
-            int count;
             if (newPrice > As2BenchConstants.MAX_PRICE){
-                count = StoredProcedureHelper.executeUpdate(
-                    "UPDATE item SET i_price = " + As2BenchConstants.MIN_PRICE + "WHERE i_id = " + iid,
-                    tx
-                );
-            } else {
-                count = StoredProcedureHelper.executeUpdate(
-                    "UPDATE item SET i_price = " + newPrice + "WHERE i_id = " + iid,
-                    tx
-                );
-            } else // TODO  fix this (double else)
-                throw new RuntimeException("Cloud not update item record with i_id = " + iid);
+                newPrice = As2BenchConstants.MIN_PRICE;
+            } 
+            int count = StoredProcedureHelper.executeUpdate(
+                "UPDATE item SET i_price = " + newPrice + "WHERE i_id = " + iid,
+                tx
+            );
+            paramHelper.setItemPrice(newPrice, idx);
         }
     }
 
